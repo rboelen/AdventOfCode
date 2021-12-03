02e57915ada62048d79724e703808a54630c205b
Richard Boelen <rboelen@devotit.nl>
2021-12-03 13:32:45 +0100

    refactor

--- 2021/Days/Day3.cs
+++ 2021/Days/Day3.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using Tidy.AdventOfCode;
+﻿using Tidy.AdventOfCode;
 
 namespace Days
 {
@@ -8,88 +7,59 @@ namespace Days
     {
         public override object ExecutePart1()
         {
-            return SolvePart1(Input);
+            if (Input == null || Input.Length < 1)
+            {
+                return 0;
+            }
+
+            var gamma = "";
+            var epsilon = "";
+
+            for (int pos = 0; pos < Input[0].Length; pos++)
+            {
+                var mc = CalcMostCommon(Input, pos);
+                gamma += mc;
+                epsilon += mc == '0' ? '1' : '0';
+            }
+
+            return Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2);
         }
 
         public override object ExecutePart2()
         {
-            return SolvePart2(Input);
-        }
-
-        public int SolvePart1(string[] input)
-        {
             if (Input == null || Input.Length < 1)
             {
                 return 0;
             }
 
-            var sampleLength = input.Length;
-            var lineLength = input[0].Length;
-            var gamma = new BitArray(lineLength);
-            var epsilon = new BitArray(lineLength);
-            var sum = new decimal[lineLength];
-
-            for (int i = 0; i < sampleLength; i++)
-            {
-                for (int j = 0; j < lineLength; j++)
-                {
-                    sum[j] += input[i][j] == '1' ? 1 : 0;
-                    gamma[lineLength - 1 - j] = sum[j] / (i + 1) >= 0.5m ? true : false;
-                    epsilon[lineLength - 1 - j] = !gamma[lineLength - 1 - j];
-                }
-            }
-
-            var result = new int[2];
-            gamma.CopyTo(result, 0);
-            epsilon.CopyTo(result, 1);
-
-            return result[0] * result[1];
-        }
-
-        public int SolvePart2(string[] input)
-        {
-            if (Input == null || Input.Length < 1)
-            {
-                return 0;
-            }
-
-            var lineLength = input[0].Length;
-
-            var oxSamples = input;
-            var co2Samples = input;
+            var oxSamples = Input;
+            var co2Samples = Input;
             var ox = 0;
             var co2 = 0;
 
-            for (int j = 0; j < lineLength; j++)
+            for (int pos = 0; pos < Input[0].Length; pos++)
             {
-                oxSamples = FilterSamples(oxSamples, j, true);
-                if (oxSamples.Length == 1)
-                {
-                    ox = Convert.ToInt32(oxSamples[0], 2);
-                }
+                oxSamples = FilterSamples(oxSamples, pos, true);
+                ox = Convert.ToInt32(oxSamples[0], 2);
 
-                co2Samples = FilterSamples(co2Samples, j, false);
-                if (co2Samples.Length == 1)
-                {
-                    co2 = Convert.ToInt32(co2Samples[0], 2);
-                }
+                co2Samples = FilterSamples(co2Samples, pos, false);
+                co2 = Convert.ToInt32(co2Samples[0], 2);
             }
 
             return ox * co2;
+        }
+
+        private static char CalcMostCommon(string[] samples, int pos)
+        {
+            decimal sum = samples.Count(s => s[pos] == '1');
+            return sum / samples.Length >= 0.5m ? '1' : '0';
         }
 
         private static string[] FilterSamples(string[] samples, int pos, bool mostCommon)
         {
             if (samples.Length > 1)
             {
-                var mc = '0';
-                decimal sum = 0;
-
-                for (int i = 0; i < samples.Length; i++)
-                {
-                    sum += samples[i][pos] == '1' ? 1 : 0;
-                    mc = sum / (i + 1) >= 0.5m ? '1' : '0';
-                }
+                var mc = CalcMostCommon(samples, pos);
 
                 if (mostCommon)
                 {
@@ -103,5 +73,6 @@ namespace Days
             }
             return samples;
         }
+
     }
 }
---
+++ 2021/Days/Parsers/BitStringToIntParser.cs
@@ -0,0 +1,12 @@
+﻿using Tidy.AdventOfCode;
+
+namespace Days.Parsers
+{
+    public class BitStringToIntParser : IParser<int[]>
+    {
+        public int[] Parse(string rawInput)
+        {
+            return rawInput.Split('\n').Select(i => Convert.ToInt32(i, 2)).ToArray();
+        }
+    }
+}
