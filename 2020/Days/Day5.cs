4de6f7e27898380249cd8d6b9519ebbef34a0357
Richard Boelen <rboelen@devotit.nl>
2021-12-05 17:31:17 +0100

    day 5

---
+++ 2020/Days/Day5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+using Tidy.AdventOfCode;
+
+namespace Days
+{
+    [Day(2020,5)]
+    internal class Day5 : Day.NewLineSplitParsed<string>
+    {
+        public override object ExecutePart1()
+        {
+            return Input.Max(x => CalcSeatId(x));
+        }
+
+        public override object ExecutePart2()
+        {
+            var seatIds = Input.Select(x => CalcSeatId(x)).ToList();
+            int min = seatIds.Min(), max = seatIds.Max();
+            return Enumerable.Range(min, max - min + 1).Except(seatIds).First();
+        }
+
+        private int CalcSeatId(string line)
+        {
+            var lineb = new string(line.Select(b => b == 'B' || b == 'R' ? '1' : '0').ToArray());
+            return Convert.ToInt32(lineb, 2);
+        }
+
+    }
+}
