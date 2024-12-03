81379d13bcee700413ad29e27bac16db2306a8dd
Richard Boelen <rboelen@devotit.nl>
2024-12-03 08:40:24 +0100

    Update day 3

--- 2024/Days.Tests/Day3Tests.cs
+++ 2024/Days.Tests/Day3Tests.cs
@@ -1,22 +1,71 @@
 using FluentAssertions;
 using NUnit.Framework;
+using System.IO;
 
 namespace Days.Tests;
 
 public class Day3Tests
 {
     [Test]
-    public void Part1_Sample_Returns_1()
+    public void Part1_Sample_Returns_161()
     {
         // arrange
-        var input = new int[][] { [7, 6, 4, 2, 1], [1, 3, 2, 4, 5] };
-        var target = new Day2 { Input = input };
+        var input = @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        var target = new Day3();
+        target.Input = input;
 
         // act
         var result = target.ExecutePart1();
 
         // assert
-        result.Should().Be(1);
+        result.Should().Be(161);
+    }
+
+    [Test]
+    public void Part1_Input_Returns()
+    {
+        // arrange
+        var input = File.ReadAllText("Resources/day3.txt");
+
+        var target = new Day3();
+        target.Input = input;
+
+        // act
+        var result = target.ExecutePart1();
+
+        // assert
+        result.Should().Be(167650499);
+    }
+
+    [Test]
+    public void Part2_Sample_Returns_48()
+    {
+        // arrange
+        var input = @"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        var target = new Day3();
+        target.Input = input;
+
+        // act
+        var result = target.ExecutePart2();
+
+        // assert
+        result.Should().Be(48);
+    }
+
+    [Test]
+    public void Part2_Input_Returns()
+    {
+        // arrange
+        var input = File.ReadAllText("Resources/day3.txt");
+
+        var target = new Day3();
+        target.Input = input;
+
+        // act
+        var result = target.ExecutePart2();
+
+        // assert
+        result.Should().Be(95846796);
     }
 
 }
--- 2024/Days/Day3.cs
+++ 2024/Days/Day3.cs
@@ -1,34 +1,63 @@
-﻿using Days.Parsers;
+﻿using System.Text.RegularExpressions;
 
 namespace Days;
 
-[Day(2024, 2)]
-internal class Day3 : Day<int[][]>
+[Day(2024, 3)]
+internal partial class Day3 : Day
 {
-    private readonly StringToIntArrayParser parser = new();
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex FindMulRegex();
 
-    public override int[][] ParseInput(string rawInput)
-    {
-        return parser.Parse(rawInput);
-    }
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")]
+    private static partial Regex FindMulRegex2();
 
     public override object ExecutePart1()
     {
-        return SolvePart1(Input);
+
+        var matches = FindMulRegex().Matches(Input);
+        var result = 0;
+        foreach (var groups in matches.Select(x => x.Groups))
+        {
+            var x = Convert.ToInt32(groups[1].Value);
+            var y = Convert.ToInt32(groups[2].Value);
+            result += x * y;
+        }
+
+
+        return result;
     }
 
     public override object ExecutePart2()
     {
-        return SolvePart1(Input);
-    }
+        // create a regex that finds mul(x,y) and do() and don't()
+        // sample input: xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        // sample output: mul(2,4), don;t(), mul(5,5), mul(11,8), do(), mul(8,5)
 
-    public static int SolvePart1(int[][] input)
-    {
-        if (input == null || input.Length == 0)
+        var matches = FindMulRegex2().Matches(Input);
+        var result = 0;
+        var enabled = true;
+        foreach (var groups in matches.Select(x => x.Groups))
         {
-            return 0;
+            if (groups[0].Value == "do()")
+            {
+                enabled = true;
+                continue;
+            }
+            if (groups[0].Value == "don't()")
+            {
+                enabled = false;
+                continue;
+            }
+
+            if (enabled)
+            {
+                var x = Convert.ToInt32(groups[1].Value);
+                var y = Convert.ToInt32(groups[2].Value);
+                result += x * y;
+            }
         }
-        return 0;
+
+        return result;
     }
 
 }
